5a3e670ee5c8fb8de556a1a043f4049a0370ed1d
fuyunguagua <425006762@qq.com>
2016-10-21 23:32:28 +0800

    Second commit

---
+++ fab.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+'''feibonaqi '''
+def fab(max):
+	n,a,b = 0,0,1
+	while n < max:
+		yield b
+		a,b = b,a+b
+		n = n + 1
+
+if __name__ == "__main__":
+	for i in fab(5):
+		print i
